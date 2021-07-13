68e5ce9fb8ef9a4c96466b8c20f13271a0d99e1a
vigneshkumar.ec17@bitsathy.ac.in <vigneshkumar.ec17@bitsathy.ac.in>
2021-07-13 19:32:28 +0530

    Conditional Compilations

---
+++ Conditional Compilation/conditionalcompilation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define ZOHO
+
+
+int main()
+{
+#ifdef ZOHO
+    printf("ZOHO is defined using preprocessing directive");
+#else
+    printf("ZOHO is not defined using preprocessing directive");
+#endif
+
+    return 0;
+}
