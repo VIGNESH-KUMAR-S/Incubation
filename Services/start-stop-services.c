8c09afc3d8138987091abe1181a04a4dd4956cfc
Vignesh Kumar S <vigneshkumar.ec17@bitsathy.ac.in>
2021-07-27 13:44:50 +0530

    Create the service for existing .exe file

--- Services/start-stop-services.c
+++ Services/start-stop-services.c
@@ -2,17 +2,16 @@
 #include <Windows.h>
 #include <winsvc.h>
 
-void closeServiceHandle(SC_HANDLE openService)
+int main()
 {
-	CloseServiceHandle(openService);
-}
-
-int main(void)
-{
+	puts(" _______________________________________________ ");
+	puts("|\tSTART AND STOP EXISTING SERVICES\t|");
+	puts("|_______________________________________________|");
+	// START AND STOP EXISTING SERVICES
 	SC_HANDLE hscManager;
 
 	//1. handle to the service control manager database
-	hscManager = OpenSCManagerA(
+	hscManager = OpenSCManager(
 		NULL, // local machine
 		NULL, // SERVICES_ACTIVE_DATABASE database is opened by default
 		SC_MANAGER_ALL_ACCESS
@@ -21,11 +20,10 @@ int main(void)
 	{
 		puts("\n\tOpenSCManager() function failed");
 		printf("\n\tError No\t: %d\n", GetLastError());
-		closeServiceHandle(hscManager);
 		return 0;
 	}
 	else
-		puts("\n\tOpenSCManager() function success");
+		puts("\n\tOpenSCManager() function success - Created handle to SCM DB");
 
 	//2. OpenService() returns handle to the service
 	SC_HANDLE openService = OpenServiceA(
@@ -37,11 +35,10 @@ int main(void)
 	{
 		puts("\n\tOpenServiceA() function failed");
 		printf("\n\tError No\t: %d\n", GetLastError());
-		closeServiceHandle(openService);
 		return 0;
 	}
 	else
-		puts("\n\tOpenServiceA() function success");
+		puts("\n\tOpenServiceA() function success - Created handle to particular service");
 
 	//3. StartService() function used to start existing services
 	int startService = StartServiceA(
@@ -53,12 +50,11 @@ int main(void)
 	{
 		puts("\n\tStartService() function failed");
 		printf("\n\tError No\t: %d\n", GetLastError());
-		closeServiceHandle(openService);
 		return 0;
 	}
 	else
-		puts("\n\tStartService() function success");
-
+		puts("\n\tStartService() function success - Particular service running");
+	puts("");
 	system("PAUSE");
 	//4. QueryServiceStatusEx() function tells status of the service
 	SERVICE_STATUS_PROCESS  serviceStatusProcess;
@@ -74,12 +70,11 @@ int main(void)
 	{
 		puts("\n\tQueryServiceStatusEx() function failed");
 		printf("\n\tError No\t: %d\n", GetLastError());
-		closeServiceHandle(openService);
 		return 0;
 	}
 	else
 	{
-		puts("\n\tQueryServiceStatusEx() function success");
+		puts("\n\tQueryServiceStatusEx() function success - Able to get the status of given service");
 		if (serviceStatusProcess.dwCurrentState == SERVICE_STOP || serviceStatusProcess.dwCurrentState == SERVICE_STOPPED)
 		{
 			puts("\n\tService Already Stopped");
@@ -87,7 +82,6 @@ int main(void)
 		else if (serviceStatusProcess.dwCurrentState == SERVICE_STOP_PENDING)
 		{
 			puts("\n\tService Stop Pending!!!!!");
-			closeServiceHandle(openService);
 			return 0;
 		}
 		else if (serviceStatusProcess.dwCurrentState == SERVICE_RUNNING)
@@ -104,11 +98,10 @@ int main(void)
 			{
 				puts("\n\tControlService() function failed");
 				printf("\n\tError No\t: %d\n", GetLastError());
-				closeServiceHandle(openService);
 				return 0;
 			}
 			else
-				puts("\n\tControlService() function success");
+				puts("\n\tControlService() function success - I used this to STOP given service");
 
 			serviceStatus = QueryServiceStatusEx(
 				openService,
@@ -121,22 +114,20 @@ int main(void)
 			{
 				puts("\n\tQueryServiceStatusEx() function failed");
 				printf("\n\tError No\t: %d\n", GetLastError());
-				closeServiceHandle(openService);
 				return 0;
 			}
 			else
 			{
-				puts("\n\tQueryServiceStatusEx() function success");
+				puts("\n\tQueryServiceStatusEx() function success - To check status of given service");
 				if (serviceStatusProcess.dwCurrentState == SERVICE_STOP_PENDING)
 				{
-					puts("\n\tService Stop Pending, Wait for more than 2 seconds");
+					puts("\n\tService Stop Pending, Wait for more than 2 seconds\n");
 					system("PAUSE");
 					if (QueryServiceStatusEx(openService, SC_STATUS_PROCESS_INFO, (LPBYTE)&serviceStatusProcess,
 						sizeof(SERVICE_STATUS_PROCESS), &dwBytesNeeded) == 0)
 					{
 						puts("\n\tQueryServiceStatusEx() function failed");
 						printf("\n\tError No\t: %d\n", GetLastError());
-						closeServiceHandle(openService);
 						return 0;
 					}
 					else
@@ -152,11 +143,85 @@ int main(void)
 				else
 				{
 					puts("\n\tService Not Stopped");
-					closeServiceHandle(openService);
 					return 0;
 				}
 			}
 		}
 	}
-	closeServiceHandle(openService);
+	CloseServiceHandle(hscManager);
+	CloseServiceHandle(openService);
+
+	puts("\n===================================================================================\n\n");
+	puts(" _______________________________________________________ ");
+	puts("|\tCREATING THE SERVICE USING EXISTING .exe FILE\t|");
+	puts("|_______________________________________________________|");
+	// CREATING THE SERVICE USING EXISTING .exe FILE
+	puts("");
+	system("PAUSE");
+
+	//1. handle to the service control manager database
+	hscManager = OpenSCManager(
+		NULL, // local machine
+		NULL, // SERVICES_ACTIVE_DATABASE database is opened by default
+		SC_MANAGER_ALL_ACCESS
+	);
+	if (hscManager == NULL)
+	{
+		puts("\n\tOpenSCManager() function failed");
+		printf("\n\tError No\t: %d\n", GetLastError());
+		return 0;
+	}
+	else
+		puts("\n\tOpenSCManager() function success - Created handle to SCM DB");
+
+	// The executable for cmd
+	LPCTSTR lpszBinaryPathName = L"%SystemRoot%\\System32\\cmd.exe";
+
+	// Service display name...
+	LPCTSTR lpszDisplayName = L"AAAA";
+
+	// Registry Subkey
+	LPCTSTR lpszServiceName = L"aaaa";
+
+	//2. CreateService() created a service and returns handle to the created service
+	SC_HANDLE createService = CreateService(
+		hscManager,			// SCManager database
+		lpszServiceName,	// name of service
+		lpszDisplayName,	// service name to display
+		SERVICE_ALL_ACCESS,	// desired access
+		SERVICE_WIN32_OWN_PROCESS,	// service type
+		SERVICE_DEMAND_START,		// start type
+		SERVICE_ERROR_NORMAL,		// error control type
+		lpszBinaryPathName,			// service's binary
+		NULL, // no load ordering group
+		NULL, // no tag identifier
+		NULL, // no dependencies
+		NULL, // LocalSystem account
+		NULL); // no password
+
+	if (createService == NULL)
+	{
+		puts("\n\tCreateService() function failed");
+		printf("\n\tError No\t: %ld\n", GetLastError());
+		return 0;
+	}
+	else
+		printf("\n\tCreateService() for %S success - Return handle to created service\n", lpszServiceName);
+
+	puts("");
+	system("PAUSE");
+
+	//3. Delete the particular service
+	if (!DeleteService(createService) && createService != NULL)
+	{
+		puts("\n\tDeleteService() function failed");
+		printf("\n\tError No\t: %d\n", GetLastError());
+	}
+	else
+		puts("\n\tService deleted successfully\n");
+
+	CloseServiceHandle(hscManager);
+	CloseServiceHandle(createService);
+
+	return 0;
 }
